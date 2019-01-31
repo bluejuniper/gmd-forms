4bbbb793bb5f09f48102f4467129f81642e0efd3
Arthur Barnes <artbarnes@ieee.org>
2019-01-30 17:33:32 -0800

    converted to matlab syntax

--- gic-matrix.jl
+++ gic-matrix.jl
@@ -23,71 +23,71 @@ println("Start solving $path")
 # is symetric and, for now, Z is diagonal.
 
 ######
-%I assume the branchListStruct and busListStruct are snatched directly from
-%jsondecode.  First I need to make these maps for somewhat easier access.
+# I assume the branchListStruct and busListStruct are snatched directly from
+# jsondecode.  First I need to make these maps for somewhat easier access.
 branchMap=net["gmd_branch"]
 busMap=net["gmd_bus"]
 
-%We will be solving for bus to ground currents.  We have two matrices to 
-%worry about, Y and Z.  Both are nxn where n is the number of busses.  Y
-%is symettric and, for now, Z is diagonal.
+# We will be solving for bus to ground currents.  We have two matrices to 
+# worry about, Y and Z.  Both are nxn where n is the number of busses.  Y
+# is symettric and, for now, Z is diagonal.
 
 numBranch=length(branchMap)
 numBus=length(busMap)
 
 
-branchList=values(branchMap)'
-busList=values(busMap)'
-busIdx=cell2mat(keys(busMap))'
+branchList=collect(values(branchMap))
+busList=collect(values(busMap))
+busIdx=Dict([(b["index"],i) for (i,b) in enumerate(busList)])
 
-%First we need JJ, which is the perfect earth grounding current at each
-%bus location.  It is the sum of the emf on each line going into a
-%substation time the y for that line
+# First we need JJ, which is the perfect earth grounding current at each
+# bus location.  It is the sum of the emf on each line going into a
+# substation time the y for that line
 
-J=zeros(numBus,1)
+J=zeros(numBus)
 
-mm=zeros(2*numBranch,1); % off diag rows
-nn=zeros(2*numBranch,1); %off diag cols
-matVals=zeros(2*numBranch,1); % off-diagonal values
+mm=zeros(Int64,2*numBranch) # off diag rows
+nn=zeros(Int64,2*numBranch) #off diag cols
+matVals=zeros(2*numBranch) # off-diagonal values
 z=-1
 
-mmm=(1:numBus)'; % diag rows
-nnn=(1:numBus)'; % diag cols
-YY=zeros(numBus,1); % diagonal values
+mmm=Array(1:numBus) # diag rows
+nnn=Array(1:numBus) # diag cols
+YY=zeros(numBus) # diagonal values
 
-for i=1:numBranch
-    branch=branchList{i}
-    m=find(busIdx==branch.f_bus,1)
-    n=find(busIdx==branch.t_bus,1)
-    if((m~=n) && (branch.br_status==1))
-        J(m) = J(m) - (1.00/branch.br_r)*branch.br_v
-        J(n) = J(n) + (1.00/branch.br_r)*branch.br_v
+for i in 1:numBranch
+    branch=branchList[i]
+    m=find(busIdx==branch["f_bus"])
+    n=find(busIdx==branch["t_bus"])
+    if((m!=n) && (branch["br_status"]==1))
+        J[m] = J[m] - (1.00/branch["br_r"])*branch["br_v"]
+        J[n] = J[n] + (1.00/branch["br_r"])*branch["br_v"]
         
         z=z+2
-        mm(z)=m
-        nn(z)=n
-        matVals(z)=-(1.00/branch.br_r)
+        mm[z]=m
+        nn[z]=n
+        matVals[z]=-(1.00/branch["br_r"])
         
-        mm(z+1)=n
-        nn(z+1)=m
-        matVals(z+1)=matVals(z)
+        mm[z+1]=n
+        nn[z+1]=m
+        matVals[z+1]=matVals[z]
         
-        YY(m) = YY(m) + (1.00/branch.br_r)
-        YY(n) = YY(n) + (1.00/branch.br_r)
+        YY[m] = YY[m] + (1.00/branch["br_r"])
+        YY[n] = YY[n] + (1.00/branch["br_r"])
     end
     
 end
 
-Y=sparse([mmm;mm],[nnn;nn],[YY;matVals])
+Y=sparse(vcat(mmm,mm),vcat(nnn,nn),vcat(YY,matVals))
 
-zmm=zeros(numBus,1)
-znn=zeros(numBus,1)
-zmatVals=zeros(numBus,1)
-for i=1:numBus
-    bus=busList{i}
-    zmm(i)=i
-    znn(i)=i
-    zmatVals(i)=(1.00/bus.g_gnd)
+zmm=zeros(Int64,numBus)
+znn=zeros(Int64,numBus)
+zmatVals=zeros(numBus)
+for i in 1:numBus
+    bus=busList[i]
+    zmm[i]=i
+    znn[i]=i
+    zmatVals[i]=(1.00/bus["g_gnd"])
 end
 Z=sparse(zmm,znn,zmatVals)
 
@@ -100,10 +100,10 @@ M=(I+MM)
 gic=M\J
 vdc=Z*gic
 
-end
 
 
 ######
+
 result["gic"] = Dict()
 result["vdc"] = Dict()
 ##end
