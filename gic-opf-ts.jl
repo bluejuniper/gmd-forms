08331ace397ce1ade30d5a06b5f663e86544eca8
Art Barnes <abarnes@lanl.gov>
2019-07-12 14:41:33 -0600

    added adams file

--- gic-opf-ts.jl
+++ gic-opf-ts.jl
@@ -1,121 +1,136 @@
-using Ipopt, PowerModels, PowerModelsGMD
-include("thermal-variable.jl")
-#include("thermal-constraint.jl")
-include("thermal-constraint-template.jl")
+using PowerModels, PowerModelsGMD, Ipopt, JuMP, JSON
+#CSV, DataFrames
+
+# include("thermal-variable.jl")
+# include("thermal-constraint.jl")
+# include("thermal-constraint-template.jl")
+
+const PMgmd = PowerModelsGMD
+
+# -- F U N C T I O N S -- #
+
+# FUNCTION: convinience function
+# function run_gic_opf_ts(file, model_constructor, solver; kwargs...)
+#     return run_model(file, model_constructor, solver, post_gic_opf_ts; multinetwork=true, kwargs...)
+# end
 
 
-""
-function run_gic_opf_ts(file, model_constructor, solver; kwargs...)
-    return run_generic_model(file, model_constructor, solver, post_gic_opf_ts; multinetwork=true, kwargs...)
-end
+# # FUNCTION: problem formulation
+# function post_gic_opf_ts(pm::GenericPowerModel)
+#     for (n, network) in nws(pm)
+#         PowerModels.variable_voltage(pm, nw=n)
+#         PowerModelsGMD.variable_dc_voltage(pm, nw=n)
+#         PowerModelsGMD.variable_dc_current_mag(pm, nw=n)
+#         PowerModelsGMD.variable_qloss(pm, nw=n)
+#         PowerModels.variable_generation(pm, nw=n)
+#         PowerModels.variable_branch_flow(pm, nw=n)
+#         PowerModels.variable_dcline_flow(pm, nw=n)
+#         PowerModelsGMD.variable_dc_line_flow(pm, nw=n)
 
-""
-function post_gic_opf_ts(pm::GenericPowerModel)
-    for (n, network) in nws(pm)
-        PowerModels.variable_voltage(pm, nw=n)
-        PowerModelsGMD.variable_dc_voltage(pm, nw=n) # OK
-        PowerModelsGMD.variable_dc_current_mag(pm, nw=n) # OK
-        PowerModelsGMD.variable_qloss(pm, nw=n) # OK
-        PowerModels.variable_generation(pm, nw=n)
-        PowerModels.variable_branch_flow(pm, nw=n)
-        PowerModels.variable_dcline_flow(pm, nw=n)
-        PowerModelsGMD.variable_dc_line_flow(pm, nw=n)
-        variable_delta_oil_ss(pm, nw=n)
-        #variable_delta_oil(pm, nw=n)
+#         variable_delta_oil_ss(pm, nw=n)
+#         variable_delta_oil(pm, nw=n)
 
-        PowerModels.constraint_voltage(pm, nw=n)
+#         PowerModels.constraint_model_voltage(pm, nw=n)
 
-        for i in ids(pm, :ref_buses, nw=n)
-            PowerModels.constraint_theta_ref(pm, i, nw=n)
-        end
+#         for i in PowerModels.ids(pm, :ref_buses, nw=n)
+#             PowerModels.constraint_theta_ref(pm, i, nw=n)
+#         end
 
-        for i in ids(pm, :bus, nw=n)
-            # TODO: see if I can use shunt_gic instead
-            PowerModelsGMD.constraint_kcl_gic(pm, i, nw=n) # OK
-        end
+#         for i in PowerModels.ids(pm, :bus, nw=n)
+#             PowerModelsGMD.constraint_kcl_gmd(pm, i, nw=n)
+#         end
 
-        for i in ids(pm, :branch, nw=n)
-            PowerModelsGMD.constraint_dc_current_mag(pm, i, nw=n) # OK
-            PowerModelsGMD.constraint_qloss_vnom(pm, i, nw=n) # OK
+#         for i in PowerModels.ids(pm, :branch, nw=n)
+#             PowerModelsGMD.constraint_dc_current_mag(pm, i, nw=n)
+#             PowerModelsGMD.constraint_qloss_vnom(pm, i, nw=n)
 
-            PowerModels.constraint_ohms_yt_from(pm, i, nw=n)
-            PowerModels.constraint_ohms_yt_to(pm, i, nw=n)
+#             PowerModels.constraint_ohms_yt_from(pm, i, nw=n)
+#             PowerModels.constraint_ohms_yt_to(pm, i, nw=n)
 
-            PowerModels.constraint_voltage_angle_difference(pm, i, nw=n)
+#             PowerModels.constraint_voltage_angle_difference(pm, i, nw=n)
 
-            PowerModels.constraint_thermal_limit_from(pm, i, nw=n)
-            PowerModels.constraint_thermal_limit_to(pm, i, nw=n)
+#             PowerModels.constraint_thermal_limit_from(pm, i, nw=n)
+#             PowerModels.constraint_thermal_limit_to(pm, i, nw=n)
 
-            constraint_temperature_state_ss(pm, i, nw=n)
-        end
+#             constraint_temperature_state_ss(pm, i, nw=n)
+#         end
 
-        ### DC network constraints ###
-        for i in ids(pm, :gmd_bus)
-            PowerModelsGMD.constraint_dc_kcl_shunt(pm, i, nw=n) # OK
-        end
+#         ### DC network constraints ###
+#         for i in PowerModels.ids(pm, :gmd_bus)
+#             PowerModelsGMD.constraint_dc_kcl_shunt(pm, i, nw=n)
+#         end
 
-        for i in ids(pm, :gmd_branch)
-            PowerModelsGMD.constraint_dc_ohms(pm, i, nw=n) # OK
-        end
+#         for i in PowerModels.ids(pm, :gmd_branch)
+#             PowerModelsGMD.constraint_dc_ohms(pm, i, nw=n)
+#         end
 
-        for i in ids(pm, :dcline, nw=n)
-            PowerModels.constraint_dcline(pm, i, nw=n)
-        end
-    end
+#         for i in PowerModels.ids(pm, :dcline, nw=n)
+#             PowerModels.constraint_dcline(pm, i, nw=n)
+#         end
+#     end
 
-    PowerModelsGMD.objective_gic_min_fuel(pm)
-end
+#     PowerModelsGMD.objective_gmd_min_fuel(pm)
+# end
 
 
+
+# -- T E S T I N G -- #
+
+println("")
+
+# Load waveform data
+println("Load waveform data\n")
 wf_path = "data/b4gic-gmd-wf.json"
 h = open(wf_path)
 wf_data = JSON.parse(h)
 close(h)
 
-t = wf_data["time"]
+timesteps = wf_data["time"]
+n = length(timesteps)
+t = range(0, stop=(3600*3*3), length=n)
+Delta_t = t[2]-t[1]
 waveforms = wf_data["waveforms"]
-n = length(t)
 
-path = "data/b4gic.m"
+
+# Load case data
+println("Load case data\n")
+path = joinpath(dirname(pathof(PowerModelsGMD)), "../test/data/b4gic.m")
 raw_net = PowerModels.parse_file(path)
-# fix an error in the data
-raw_net["name"] = "b4gic"
-raw_net["branch"]["2"]["transformer"] = false
-@printf "Running model %s\n" raw_net["name"]
+raw_net["name"] = "B4GIC"
+base_mva = raw_net["baseMVA"]
+println("")
 
-ipopt_solver = IpoptSolver(tol=1e-6, print_level=0)
-setting = Dict{AbstractString,Any}("output" => Dict{AbstractString,Any}("line_flows" => true))
+
+# Running model
+println("Running model: $(raw_net["name"]) \n")
+ipopt_solver = JuMP.with_optimizer(Ipopt.Optimizer, tol=1e-6, print_level=0)
+setting = Dict{String,Any}("output" => Dict{String,Any}("branch_flows" => true))
+
 results = []
 
-mn = Dict{String,Any}(
-    "name" => "$(raw_net["name"])_ts",
-    "multinetwork" => true,
-    "per_unit" => raw_net["per_unit"],
-    "baseMVA" => raw_net["baseMVA"],
-    "nw" => Dict{String,Any}()
-)
+# Create replicates (multiples) of the network
+net = PowerModels.replicate(raw_net,n)
 
-ni = 1 # network index
-for i in 1:10:n
-    @printf "\n\n########## Time: %0.3f ##########\n" t[i]
-    net = deepcopy(raw_net)
 
-    # update the vs values
-    for (k,wf) in waveforms
-        otype = wf["parent_type"]
-        field  = wf["parent_field"]
-        net[otype][k][field] = wf["values"][i]
+# Update values in each replicates
+for repl in keys(net["nw"])
+
+    for i in 1:n
+        #println("########## Time: $(t[i]) ########## \n")
+
+        #update the vs values
+        for (k,wf) in waveforms
+            otype = wf["parent_type"]
+            field  = wf["parent_field"]
+            net["nw"][repl][otype][k][field] = wf["values"][i]
+        end        
+
     end
 
-    delete!(net, "multinetwork")
-    delete!(net, "per_unit")
-    delete!(net, "baseMVA")
-    mn["nw"]["$ni"] = net
-
-    ni += 1
 end
 
-run_gic_opf_ts(mn, PowerModelsGMD.ACPPowerModel, ipopt_solver)
+result = run_gmd_opf_ts(net, PowerModelsGMD.ACPPowerModel, ipopt_solver, setting=setting)
+
 
 
 #outfile = string("data/", net["name"], "_result.json")
@@ -124,7 +139,4 @@ run_gic_opf_ts(mn, PowerModelsGMD.ACPPowerModel, ipopt_solver)
 #JSON.print(f,results)
 #close(f)    
  
-
-
-
 
