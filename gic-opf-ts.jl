4bd15a013af3e70e1fdbb7a6d9d1bdbed1078e40
Art Barnes <abarnes@lanl.gov>
2019-07-12 14:43:07 -0600

    removed commented stuff

--- gic-opf-ts.jl
+++ gic-opf-ts.jl
@@ -1,82 +1,4 @@
 using PowerModels, PowerModelsGMD, Ipopt, JuMP, JSON
-#CSV, DataFrames
-
-# include("thermal-variable.jl")
-# include("thermal-constraint.jl")
-# include("thermal-constraint-template.jl")
-
-const PMgmd = PowerModelsGMD
-
-# -- F U N C T I O N S -- #
-
-# FUNCTION: convinience function
-# function run_gic_opf_ts(file, model_constructor, solver; kwargs...)
-#     return run_model(file, model_constructor, solver, post_gic_opf_ts; multinetwork=true, kwargs...)
-# end
-
-
-# # FUNCTION: problem formulation
-# function post_gic_opf_ts(pm::GenericPowerModel)
-#     for (n, network) in nws(pm)
-#         PowerModels.variable_voltage(pm, nw=n)
-#         PowerModelsGMD.variable_dc_voltage(pm, nw=n)
-#         PowerModelsGMD.variable_dc_current_mag(pm, nw=n)
-#         PowerModelsGMD.variable_qloss(pm, nw=n)
-#         PowerModels.variable_generation(pm, nw=n)
-#         PowerModels.variable_branch_flow(pm, nw=n)
-#         PowerModels.variable_dcline_flow(pm, nw=n)
-#         PowerModelsGMD.variable_dc_line_flow(pm, nw=n)
-
-#         variable_delta_oil_ss(pm, nw=n)
-#         variable_delta_oil(pm, nw=n)
-
-#         PowerModels.constraint_model_voltage(pm, nw=n)
-
-#         for i in PowerModels.ids(pm, :ref_buses, nw=n)
-#             PowerModels.constraint_theta_ref(pm, i, nw=n)
-#         end
-
-#         for i in PowerModels.ids(pm, :bus, nw=n)
-#             PowerModelsGMD.constraint_kcl_gmd(pm, i, nw=n)
-#         end
-
-#         for i in PowerModels.ids(pm, :branch, nw=n)
-#             PowerModelsGMD.constraint_dc_current_mag(pm, i, nw=n)
-#             PowerModelsGMD.constraint_qloss_vnom(pm, i, nw=n)
-
-#             PowerModels.constraint_ohms_yt_from(pm, i, nw=n)
-#             PowerModels.constraint_ohms_yt_to(pm, i, nw=n)
-
-#             PowerModels.constraint_voltage_angle_difference(pm, i, nw=n)
-
-#             PowerModels.constraint_thermal_limit_from(pm, i, nw=n)
-#             PowerModels.constraint_thermal_limit_to(pm, i, nw=n)
-
-#             constraint_temperature_state_ss(pm, i, nw=n)
-#         end
-
-#         ### DC network constraints ###
-#         for i in PowerModels.ids(pm, :gmd_bus)
-#             PowerModelsGMD.constraint_dc_kcl_shunt(pm, i, nw=n)
-#         end
-
-#         for i in PowerModels.ids(pm, :gmd_branch)
-#             PowerModelsGMD.constraint_dc_ohms(pm, i, nw=n)
-#         end
-
-#         for i in PowerModels.ids(pm, :dcline, nw=n)
-#             PowerModels.constraint_dcline(pm, i, nw=n)
-#         end
-#     end
-
-#     PowerModelsGMD.objective_gmd_min_fuel(pm)
-# end
-
-
-
-# -- T E S T I N G -- #
-
-println("")
 
 # Load waveform data
 println("Load waveform data\n")
@@ -132,11 +54,4 @@ end
 result = run_gmd_opf_ts(net, PowerModelsGMD.ACPPowerModel, ipopt_solver, setting=setting)
 
 
-
-#outfile = string("data/", net["name"], "_result.json")
-#println("\nSaving results to $outfile")
-#f = open(outfile,"w")
-#JSON.print(f,results)
-#close(f)    
- 
 
