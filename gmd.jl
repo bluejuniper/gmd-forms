eb8b0ab11f6a32b242c3385b4dd14c9d1f68aca5
Art Barnes <abarnes@lanl.gov>
2019-07-18 09:23:36 -0600

    tightened bounds on rts-gmlc-gic

--- gmd.jl
+++ gmd.jl
@@ -3,9 +3,10 @@ include("powermodelsio.jl")
 
 println("Start loading json")
 path = "data/rts_gmlc_gic.m"
-opath = "data/rts-gmlc-gic-matpower-results.json"
 
-
+if length(ARGS) >= 1
+    path = ARGS[1]
+end
 
 println("Start loading $path")
 if endswith(path, ".m") || endswith(path, ".raw")
@@ -25,13 +26,29 @@ println("Done loading $path")
 net["storage"] = Dict()
 PowerModels.make_per_unit!(net)
 
-ipopt_solver = JuMP.with_optimizer(Ipopt.Optimizer, tol=1e-6, print_level=0)
+solver = JuMP.with_optimizer(Ipopt.Optimizer, tol=1e-6, print_level=0)
+s = Dict{String,Any}("output" => Dict{String,Any}("line_flows" => true))
 println("Start solving")
-setting = Dict{String,Any}("output" => Dict{String,Any}("branch_flows" => true))
-# result = run_gmd(net, ipopt_solver; setting=setting)
-result = run_ac_gmd_opf_decoupled(net, ipopt_solver; setting=setting)
+
+result = run_gmd(net, solver; setting=s)
+
+ipath = replace(path,".json" => "_gi.json")
+ipath = replace(ipath, ".m" => "_gic.json")
+h = GZip.open(ipath,"w")
+JSON.print(h, net)
+close(h)
 
 
-# io = open("data/rts_gmlc_gic.m", "w")
-# PowerModels.export_matpower(io, net)
-# close(io)
+
+opath = replace(path,".json" => "_gic_results.json")
+opath = replace(opath, ".m" => "_gic_results.json")
+h = GZip.open(opath,"w")
+JSON.print(h, result)
+close(h)
+
+
+ 
+
+
+
+
--- opf.jl
+++ opf.jl
@@ -3,6 +3,7 @@ include("powermodelsio.jl")
 
 println("Start loading json")
 path = "data/rts_gmlc_gic.m"
+path = "data/rts_gmlc_gic_new.m"
 
 if length(ARGS) >= 1
     path = ARGS[1]
@@ -30,21 +31,16 @@ solver = JuMP.with_optimizer(Ipopt.Optimizer, tol=1e-6, print_level=0)
 s = Dict{String,Any}("output" => Dict{String,Any}("line_flows" => true))
 println("Start solving")
 
-output = run_ac_gmd_opf_decoupled(net, solver; setting=s)
+result = run_ac_opf(net, solver; setting=s)
 
-ipath = replace(path,".json" => "_gic_opf_decoupled.json")
-ipath = replace(ipath, ".m" => "_gic_opf_decoupled.json")
-h = GZip.open(ipath,"w")
-JSON.print(h, net)
-close(h)
-
+# opath = replace(ipath, ".m" => "_opf.json")
+# h = GZip.open(opath, "w")
+# JSON.print(h, result)
+# close(h)
 
 
-opath = replace(path,".json" => "_gic_opf_decoupled_results.json")
-opath = replace(opath, ".m" => "_gic_opf_decoupled_results.json")
-h = GZip.open(opath,"w")
-JSON.print(h, result)
-close(h)
+
+
 
 
  
